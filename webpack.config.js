939023066d1d1fda5f43d3c0af6468abe424128a
Serhii Puchkovskii <serhiipuchkovskii@gmail.com>
2019-07-23 15:49:34 +0300

    Add babel and necessary presets.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,34 @@
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
+  
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    library: 'sp'
+  },
+
+  optimization: {
+    minimize: NODE_ENV != 'development'
+  },
+
+  devtool: NODE_ENV == 'development' ? 'source-map' : null,
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [
+          path.resolve(__dirname, 'src')
+        ],
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
   }
 };
