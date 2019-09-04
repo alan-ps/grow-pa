fca9a7f49997fc6ff5fb841f8620ad8fae9b00ae
Serhii Puchkovskii <serhiipuchkovskii@gmail.com>
2019-09-04 17:22:02 +0300

    Add a display action component.

---
+++ src/components/Actions/DisplayAction.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+/**
+ * Represents a display action component.
+ */
+class DisplayAction extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.handleDisplay = this.handleDisplay.bind(this);
+  }
+
+  handleDisplay(event, displayType) {
+    event.preventDefault();
+
+    this.props.dispatch({ type: 'DISPLAY_CHANGE', display: displayType});
+  }
+
+  render() {
+    const displayTypes = { bars: 'fa-th', list: 'fa-list'};
+
+    return Object.keys(displayTypes).map(
+      item => <Link
+        to = '#'
+        className = { `category-cp__action-item fa ${ displayTypes[item] }` }
+        onClick = { event => this.handleDisplay(event, item) }
+      />
+    );
+  }
+
+}
+
+const mapStateToProps = state => ({ ...state.actionReducer });
+export default connect(mapStateToProps)(DisplayAction);
