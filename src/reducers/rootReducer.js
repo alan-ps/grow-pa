3792b97c9764ede0699a8801cd0b17b4eb7c4f8e
alan-ps <serhiipuchkovskii@gmail.com>
2019-09-25 23:26:11 +0300

    Include goal reducer to the root one.

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -3,12 +3,14 @@ import { combineReducers } from 'redux';
 import modalReducer from './modalReducer';
 import restReducer from './restReducer';
 import actionReducer from './actionReducer';
+import goalReducer from './goalReducer';
 
 // The main application reducer.
 const RootReducer = combineReducers({
   modalReducer,
   restReducer,
-  actionReducer
+  actionReducer,
+  goalReducer
 });
 
 export default RootReducer;
