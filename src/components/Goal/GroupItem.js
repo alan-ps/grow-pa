463ce4fed9ec1b2cd4cbae0c7175ca0c15ee2554
alan-ps <serhiipuchkovskii@gmail.com>
2019-09-26 21:16:24 +0300

    Use the react states instead of details tag.

--- src/components/Goal/GroupItem.js
+++ src/components/Goal/GroupItem.js
@@ -4,17 +4,42 @@ import GoalItem from 'components/Goal/GoalItem';
 /**
  * Represents a single group item.
  */
-const GroupItem = (props) => (
-  <details className="goal-list_group" open>
-    <summary>{props.name}</summary>
-    {
-      props.goals.map(goal => (
-        goal.settings.group == props.id ? (
-          <GoalItem key={ goal.id } name={ goal.value } />
-        ) : false
-      ))
+class GroupItem extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      visibility: false
     }
-  </details>
-);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    this.setState({visibility: !this.state.visibility})
+  }
+
+  render() {
+    const groupVisibility = this.state.visibility ? 'hidden' : 'active';
+
+    return (
+      <React.Fragment>
+        <div className="goal-list__group" onClick={this.handleClick}>
+          {this.props.name}
+        </div>
+        <div className={`goal-list__items goal-list__items--${groupVisibility}`} >
+          {
+            this.props.goals.map(goal => (
+              goal.settings.group == this.props.id ? (
+                <GoalItem key={ goal.id } name={ goal.value } />
+              ) : false
+            ))
+          } 
+        </div>
+      </React.Fragment>
+    )
+  }
+}
 
 export default GroupItem;
